336ab3949e1d387c68c7250c6881ce245778889c
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2024-05-06 17:19:49 +0530

    server changes for deployment

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin: "https://localhost:3000",
+        origin: "https://aeonlogs.onrender.com",
         credentials: true
     })
 )
