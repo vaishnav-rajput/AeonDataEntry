a5019556d6defd4f8a30b4cc32d75e8d40ee00cd
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2024-05-06 17:45:40 +0530

    changed origin

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin: "https://aeonlogs.onrender.com/api/v1",
+        origin: "https://aeonlogs.onrender.com",
         credentials: true
     })
 )
