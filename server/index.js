337900f63c000069bccf8631222454a21c6fd3ee
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2024-05-08 14:38:34 +0530

    origin changed to vercel link

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://aeonlogs.onrender.com",
         credentials: true
     })
 )
