e9c9f4a9ab4771cb9ec3f49bd4657da420830c7b
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2024-05-08 14:48:35 +0530

    server origin changed back to 3000

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin: "https://aeonlogs.onrender.com",
+        origin: "http://localhost:3000",
         credentials: true
     })
 )
